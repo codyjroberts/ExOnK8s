961f0215b126d8d806d71c3334a51ff6b3df21b6
Cody Roberts <codyjroberts@gmail.com>
2017-11-05 21:06:47 -0600

    Wait for postgres in `pre_start.sh`

--- script/pre_start.sh
+++ script/pre_start.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# echo "Waiting for PostgreSQL to start"
-# until (echo >/dev/tcp/localhost/5432) &>/dev/null; do
-  # sleep 2
-# done
+echo "Waiting for PostgreSQL to start"
+until (echo >/dev/tcp/localhost/5432) &>/dev/null; do
+  sleep 2
+done
 
 if [ "${RUN_MIGRATIONS}" == "true" ]; then
   ./bin/exonk8s migrate
